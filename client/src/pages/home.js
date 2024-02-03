8f4a71811421b342506bf8c5f0c31a5a87ba614e
yuranay <cgule398@gmail.com>
2024-02-04 08:09:19 +0900

    swiper,material icon導入/image画像追加/サイズで表示枚数変化適用

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,14 +1,19 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import axios from 'axios'
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
-const Dashboard = () => {
+const Home = () => {
+    const [movies, setMovies] = useState([])
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const response = await axios.get('api/getPopularMovies')
-                console.log(response)
+                // console.log(response.data.results)
+                setMovies(response.data.results)
+                console.log(movies)
             } catch (err) {
                 console.log(err)
             }
@@ -27,17 +32,43 @@ const Dashboard = () => {
                 <title>Laravel - Home</title>
             </Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={swiper => console.log(swiper)}
+                breakpoints={{
+                    //320px以上の画面サイズの場合
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    //480px以上の画面サイズの場合
+                    480: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    //640px以上の画面サイズの場合
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    //768px以上の画面サイズの場合
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 40,
+                    },
+                }}>
+                {movies.map(movie => (
+                    <SwiperSlide key={movie.id}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </AppLayout>
     )
 }
 
-export default Dashboard
+export default Home
