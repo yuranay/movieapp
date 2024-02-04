041d6666ce57c14c2a5ada7fe6dbae082c8ec352
yuranay <cgule398@gmail.com>
2024-02-04 16:16:44 +0900

    tvのタイトルと公開日を表示

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import Head from 'next/head'
 import React from 'react'
 
-const Detail = ({ detail }) => {
+const Detail = ({ detail, media_type }) => {
     console.log(detail)
 
     return (
@@ -70,11 +70,13 @@ const Detail = ({ detail }) => {
                         </Grid>
                         <Grid item md={8}>
                             <Typography variant="h4" paragraph>
-                                {detail.title}
+                                {detail.title || detail.name}
                             </Typography>
                             <Typography>{detail.overview}</Typography>
                             <Typography variant="h6">
-                                公開日:{detail.release_date}
+                                {media_type == 'movie'
+                                    ? `公開日:${detail.release_date}`
+                                    : `初回放送日:${detail.first_air_date}`}
                             </Typography>
                         </Grid>
                     </Grid>
@@ -102,7 +104,7 @@ export async function getServerSideProps(context) {
         }
 
         return {
-            props: { detail: combinedData },
+            props: { detail: combinedData, media_type, media_id },
         }
     } catch {
         return { notFound: true }
