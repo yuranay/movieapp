8ade26f3354913cb65352822bf3b78373fb2a09f
yuranay <cgule398@gmail.com>
2024-02-04 14:36:48 +0900

    検索結果取得

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const search = () => {
+    const [results, setResults] = useState([])
     const router = useRouter()
     const { query: searchQuery } = router.query
     console.log(searchQuery)
@@ -17,6 +18,16 @@ const search = () => {
                     `api/searchMedia?searchQuery=${searchQuery}`,
                 )
                 console.log(response)
+                const searchResults = response.data.results
+                console.log(searchResults)
+
+                const validResults = searchResults.filter(
+                    item =>
+                        item.media_type == 'movie' || item.media_type == 'tv',
+                )
+                console.log(validResults)
+                setResults(validResults)
+                console.log(results)
             } catch (err) {
                 console.log(err)
             }
