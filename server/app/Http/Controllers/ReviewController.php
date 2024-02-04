4496ff6f17c43aafb4994a4b1990271aa5156fcd
yuranay <cgule398@gmail.com>
2024-02-04 17:04:52 +0900

    作品に紐づいたレビューだけ取得

--- server/app/Http/Controllers/ReviewController.php
+++ server/app/Http/Controllers/ReviewController.php
@@ -10,9 +10,14 @@ class ReviewController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($media_type, $media_id)
     {
-        //
+        $reviews = Review::with('user')
+            ->where('media_type', $media_type)
+            ->where('media_id', $media_id)
+            ->get();
+
+        return response()->json($reviews);
     }
 
     /**
