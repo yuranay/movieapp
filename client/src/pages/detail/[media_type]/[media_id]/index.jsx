c0a2d541fdb2bf5f4505162f30a6979492edf3ca
yuranay <cgule398@gmail.com>
2024-02-04 11:34:46 +0900

    作品詳細ページにヘッダー追加

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,67 +1,86 @@
+import AppLayout from '@/components/Layouts/AppLayout'
 import { Box, Container, Grid, Typography } from '@mui/material'
 import axios from 'axios'
+import Head from 'next/head'
 import React from 'react'
 
 const Detail = ({ detail }) => {
     console.log(detail)
 
     return (
-        <Box
-            sx={{
-                height: { xs: 'auto', md: '70vh' },
-                bgcolor: 'red',
-                position: 'relative',
-                display: 'flex',
-                alignItems: 'center',
-            }}>
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Detail
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - Detail</title>
+            </Head>
+
+            {/* 背景画像表示   */}
             <Box
                 sx={{
-                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${detail.backdrop_path})`,
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-
-                    '&::before': {
-                        content: '""',
+                    height: { xs: 'auto', md: '70vh' },
+                    bgcolor: 'red',
+                    position: 'relative',
+                    display: 'flex',
+                    alignItems: 'center',
+                }}>
+                {/* 作品画像表示 */}
+                <Box
+                    sx={{
+                        backgroundImage: `url(https://image.tmdb.org/t/p/original/${detail.backdrop_path})`,
                         position: 'absolute',
                         top: 0,
                         bottom: 0,
                         left: 0,
                         right: 0,
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                        backdropFilter: 'blur(10px)',
-                    },
-                }}
-            />
-            <Container sx={{ zIndex: 1 }}>
-                <Grid container sx={{ color: 'white' }} alignItems={'center'}>
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+
+                        '&::before': {
+                            content: '""',
+                            position: 'absolute',
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                            backdropFilter: 'blur(10px)',
+                        },
+                    }}
+                />
+                {/* 作品詳細表示 */}
+                <Container sx={{ zIndex: 1 }}>
                     <Grid
-                        item
-                        md={4}
-                        sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <img
-                            width={'70%'}
-                            src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
-                            alt=""
-                        />
+                        container
+                        sx={{ color: 'white' }}
+                        alignItems={'center'}>
+                        <Grid
+                            item
+                            md={4}
+                            sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <img
+                                width={'70%'}
+                                src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+                                alt=""
+                            />
+                        </Grid>
+                        <Grid item md={8}>
+                            <Typography variant="h4" paragraph>
+                                {detail.title}
+                            </Typography>
+                            <Typography>{detail.overview}</Typography>
+                            <Typography variant="h6">
+                                公開日:{detail.release_date}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item md={8}>
-                        <Typography variant="h4" paragraph>
-                            {detail.title}
-                        </Typography>
-                        <Typography>{detail.overview}</Typography>
-                        <Typography variant="h6">
-                            公開日:{detail.release_date}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+        </AppLayout>
     )
 }
 
